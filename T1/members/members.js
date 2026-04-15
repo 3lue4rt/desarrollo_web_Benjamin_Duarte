80495d444df3a6a67b92f1876ec5a39f1f33a6fe
Benjamin Duarte <bduartenass@gmail.com>
2026-04-14 23:40:57 -0400

    final commit?

--- T1/members/members.js
+++ T1/members/members.js
@@ -24,12 +24,38 @@ const personToRow = (person) => {
     return tr
 }
 
-//void->void
+//(db{}->db{}) -> void
 //rellena la tabla con la base de datos
-const fillTable = () => {
+const fillTable = (filter) => {
     let table = document.getElementById("table")
-    for (const data in db) {
-        table.appendChild(personToRow(db[data]))
+    for (const data of filter(db)) {
+        table.appendChild(personToRow(data))
     }
 }
 
+let table = document.getElementById("table")
+    for (const data in db) {
+        table.appendChild(personToRow(db[data]))
+    }
+
+//str -> db{} -> db{}
+const filterByType = (tipo) => (db) => {
+    return db.filter((person) => person["tipo"] == tipo)
+}
+
+const filterButton = (event) => {
+    document.getElementById("table").remove()
+    document.getElementById("table-div").innerHTML = '<table id="table" class="member-table"></table>'
+
+    let type = document.getElementById("member-type").value
+    if (type=="") {
+        fillTable((x)=>x)
+    } else {
+        fillTable(filterByType(type))
+    }
+
+    event.preventDefault()
+}
+
+let button = document.getElementById("filtrar")
+button.addEventListener("click", filterButton)
