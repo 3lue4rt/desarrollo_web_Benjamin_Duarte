1667c496e98c226361998336a5a250ff4fe1bacb
Benjamin Duarte <bduartenass@gmail.com>
2026-06-28 17:41:50 -0400

    springboot init

---
+++ T4/calidaddevida/src/main/java/com/calidaddevida/calidaddevida/CalidaddevidaApplication.java
@@ -0,0 +1,13 @@
+package com.calidaddevida.calidaddevida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalidaddevidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalidaddevidaApplication.class, args);
+	}
+
+}
---
+++ T4/calidaddevida/src/test/java/com/calidaddevida/calidaddevida/CalidaddevidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calidaddevida.calidaddevida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalidaddevidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
